1e5219060be65625bffdb7dd8514b5d2f7c93324
Muhammad Abubakar <brandmirza702@gmail.com>
2026-05-31 14:48:28 +0500

    head tail

--- head-tail.py
+++ head-tail.py
@@ -3,3 +3,8 @@ import pandas as pd
 df=pd.read_csv("Job_Placement_Data.csv")
 
 print(df.head()) #This is for reading first 5 rows 
+
+#This is for reading end 5 rows
+print("Last five rows of data")
+
+print(df.tail())
