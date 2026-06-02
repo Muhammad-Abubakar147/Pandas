94a795588b8567d018f5982669aa142f64023015
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-02 20:16:54 +0500

    structure of data

---
+++ shape.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+DATA = {
+    "NAME": ["muhammad", "abubakar" , "ammar"],
+    "age": [21, 32 ,32],
+    "city": ["faisalabad", "samnabad"  ,"lahore"]
+}
+
+df = pd.DataFrame(DATA)
+print(df)
