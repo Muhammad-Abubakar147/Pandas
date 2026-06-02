bd612b8a400ed58d1d6527cc8f0a4ad438d42fe5
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-02 19:50:59 +0500

    basics of pandas

--- Basic-of-pandas/Basic.py
+++ Basic-of-pandas/Basic.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 DATA = {
     "NAME": ["muhammad", "abubakar" , "ammar"],
-    "age": [21, 32 ,32],
+    "age": [21, 32 ,32,],
     "city": ["faisalabad", "samnabad"  ,"lahore"]
 }
 
