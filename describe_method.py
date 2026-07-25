f8a4c85fdb8727522c0b3894d140ca940ece0b01
Muhammad Abubakar <brandmirza702@gmail.com>
2026-07-25 15:59:23 +0500

    Describe method

---
+++ describe_method.py
@@ -0,0 +1,11 @@
+import pandas as pd 
+
+df=pd.read_csv("Job_Placement_Data.csv")
+
+print(df) #It will show all data in job placement csv file
+
+print("Job Placement Data Describe")
+
+print(df.describe()) #It will describe all about data in job placement
+
+#Describe data method shows (count,mean,standard deviations,minimum,75%,and max of data )
