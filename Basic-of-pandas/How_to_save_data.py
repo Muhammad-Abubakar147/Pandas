dd47bb178d06c089cc0af405909c4e84af3987e5
Muhammad Abubakar <brandmirza702@gmail.com>
2026-07-25 15:57:23 +0500

    How to save data

---
+++ Basic-of-pandas/How_to_save_data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+data ={
+    "Name":["Abubakar","Ammar","Hamza"],
+    "Age":[21,19,19],
+    "City":["Fiaslabad","Samnabad","Nvolty pull"],
+}
+df=pd.DataFrame(data)
+print(df)
+
+df.to_csv("output.csv",index=False)  #This is method to save data =>in csv file 
+df.to_excel("output.xlsx",index=False) #This is saving in excel form 
+df.to_json("output.json",index=True) #This is for saving in jason form 
