a2a87e2c1d1f6d4f93d7f624b354d02f8828b086
Muhammad Abubakar <brandmirza702@gmail.com>
2026-07-31 14:30:19 +0500

    Data frame

--- 02.Intermediate-of-Pandas/Fitering-of-columns.py
+++ 02.Intermediate-of-Pandas/Fitering-of-columns.py
@@ -1,2 +1,15 @@
 #here we will disscus how to filter a single column and multiple columns in pandas ..
 
+import pandas as pd 
+
+
+data ={
+    "Name":["Abubakar","Ammar","Hamza","Moiz Ahmad","Zubair","Faizan","Fahad"],
+    "Age":[21,19,19,23,24,25,26],
+    "City":["Fiaslabad","Samnabad","Nvolty pull","Samanabad","Samanabad","Samanabad","Samanabad"],
+    "Salary":[20000,30000,40000,50000,60000,70000,350000]
+}
+
+df=pd.DataFrame(data)
+
+print(df)
