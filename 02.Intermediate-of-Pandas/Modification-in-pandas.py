704c87e9a4456f1956cf5c55934c6e2e2f31e2fc
Muhammad Abubakar <brandmirza702@gmail.com>
2026-08-02 18:13:09 +0500

    Bonus Added

--- 02.Intermediate-of-Pandas/Modification-in-pandas.py
+++ 02.Intermediate-of-Pandas/Modification-in-pandas.py
@@ -1,1 +1,20 @@
 #Here we will disscuss how to modified new things in pandas like how to add new columns how to add rows and how to remove both and many things .
+
+
+import pandas as pd 
+
+data ={
+    "Name":["Abubakar","Ammar","Hamza","Moiz Ahmad","Zubair","Faizan","Fahad"],
+    "Age":[21,19,19,23,24,25,26],
+    "City":["Fiaslabad","Samnabad","Nvolty pull","Samanabad","Samanabad","Samanabad","Samanabad"],
+    "Salary":[20000,30000,40000,50000,60000,70000,350000]
+}
+
+df=pd.DataFrame(data)
+
+#first method is assingment method and other is insert method 
+
+#by Assingment method new column will add at end of the data set
+
+df["Bonus"]=df["Salary"]*0.1
+print(df) #it will add at the end of the column not at fvrt index 
