e91ea69ff6f9b71416e0648654d9e471ac92af7e
Muhammad Abubakar <brandmirza702@gmail.com>
2026-08-02 18:21:06 +0500

    updating method in pandas

---
+++ 02.Intermediate-of-Pandas/Updating-in-pandas.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+
+data ={
+    "Name":["Abubakar","Ammar","Hamza","Moiz Ahmad","Zubair","Faizan","Fahad"],
+    "Age":[21,19,19,23,24,25,26],
+    "City":["Fiaslabad","Samnabad","Nvolty pull","Samanabad","Samanabad","Samanabad","Samanabad"],
+    "Salary":[20000,30000,40000,50000,60000,70000,350000]
+}
+
+df=pd.DataFrame(data)
+
+#syntax for updating things in pandas 
+# df.loc[row_index,"column_name"]=New_value add 
