f88791b2c03ea2b2667b6f857091a6efcfe8ec8c
Muhammad Abubakar <brandmirza702@gmail.com>
2026-08-03 22:00:58 +0500

    Missing data detail

--- 03.Advance-of-Pandas/Handling-Missing-Data.py
+++ 03.Advance-of-Pandas/Handling-Missing-Data.py
@@ -3,13 +3,17 @@
 import pandas as pd 
 
 data ={
-    "Name":["Abubakar","Ammar","Hamza","Moiz Ahmad","Zubair","Faizan","Fahad"],
-    "Age":[21,19,19,23,24,25,26],
-    "City":["Fiaslabad","Samnabad","Nvolty pull","Samanabad","Samanabad","Samanabad","Samanabad"],
-    "Salary":[20000,30000,40000,50000,60000,70000,350000]
+    "Name":["Abubakar",None,"Hamza","Moiz Ahmad","Zubair","Faizan","Fahad"],
+    "Age":[21,None,19,23,24,25,26],
+    "City":["Fiaslabad",None,"Nvolty pull","Samanabad","Samanabad","Samanabad","Samanabad"],
+    "Salary":[20000,None,40000,50000,60000,70000,350000]
 }
 
 df=pd.DataFrame(data)
-
+print("It will print False in output")
 #syntax for detect missing data in data 
-print(df.isnull()) #will print(falseif data is not missing)
+print(df.isnull()) #will print(falseif data is not missing) and print(true) if data is missing .
+print("It will print All about data that how much data is missing")
+#And if uh want to detect how much data is missed in dataset then this method is used for it .
+print(df.isnull().sum())
+
