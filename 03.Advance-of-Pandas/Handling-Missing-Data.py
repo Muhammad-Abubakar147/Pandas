06d86d189a0ade8cc726b01d128ec3202e7949ff
Muhammad Abubakar <brandmirza702@gmail.com>
2026-08-03 22:02:22 +0500

    Output on terminal

--- 03.Advance-of-Pandas/Handling-Missing-Data.py
+++ 03.Advance-of-Pandas/Handling-Missing-Data.py
@@ -17,3 +17,21 @@ print("It will print All about data that how much data is missing")
 #And if uh want to detect how much data is missed in dataset then this method is used for it .
 print(df.isnull().sum())
 
+
+
+print("this will output on terminal")
+# It will print False in output
+#     Name    Age   City  Salary
+# 0  False  False  False   False
+# 1   True   True   True    True
+# 2  False  False  False   False
+# 3  False  False  False   False
+# 4  False  False  False   False
+# 5  False  False  False   False
+# 6  False  False  False   False
+# It will print All about data that how much data is missing
+# Name      1
+# Age       1
+# City      1
+# Salary    1
+# dtype: int64
