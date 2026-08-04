5dca3a3bdef9d370930165e2ab0ca4410a43f948
Muhammad Abubakar <brandmirza702@gmail.com>
2026-08-04 15:22:08 +0500

    output

--- 03.Advance-of-Pandas/Handling Missing Data/bfill-method.py
+++ 03.Advance-of-Pandas/Handling Missing Data/bfill-method.py
@@ -11,3 +11,21 @@ df["Marks"] = df["Marks"].bfill()
 
 print("\nAfter bfill():") #bfill stands for backword fill method 
 print(df)
+
+
+#output
+# Original DataFrame:
+#    Marks
+# 0   80.0
+# 1    NaN
+# 2    NaN
+# 3   90.0
+# 4   95.0
+
+# After bfill():
+#    Marks
+# 0   80.0
+# 1   90.0
+# 2   90.0
+# 3   90.0
+# 4   95.0
