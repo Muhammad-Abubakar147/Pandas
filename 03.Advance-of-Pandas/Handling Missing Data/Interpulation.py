cef1416c3ab7c5968f41625bb7a3c20b509b6c0a
Muhammad Abubakar <brandmirza702@gmail.com>
2026-08-04 15:11:50 +0500

    Interpolation in pandas

--- 03.Advance-of-Pandas/Handling Missing Data/Interpulation.py
+++ 03.Advance-of-Pandas/Handling Missing Data/Interpulation.py
@@ -1,1 +1,13 @@
 #Interpulation is a technique which give estimated value for example if our data is 10,20,None,40,50 ,Then this method give us 30 which is estimated value for it .
+#There many methods for interpolaton in pandas ike ,linear,polynomial,time etc .
+
+import pandas as pd
+
+df = pd.DataFrame({
+    "Marks": [50, 60, None, None, 90]
+})
+
+print(df)
+
+df["Marks"]=df["Marks"].interpolate()
+print(df)
