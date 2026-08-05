8cf950ac5fbae98db9b89b3e574ecf5c1da10e04
Muhammad Abubakar <brandmirza702@gmail.com>
2026-08-05 14:54:04 +0500

    Agregassion

--- 03.Advance-of-Pandas/Sorting and Agression/Agregassion.py
+++ 03.Advance-of-Pandas/Sorting and Agression/Agregassion.py
@@ -1,1 +1,18 @@
 #Agreegassion in pandas is used for the calculate summary for example : mean,sum,and many others .
+
+
+
+import pandas as pd 
+
+
+data ={
+    "Name":["Omer","zabiullah","Hassan"],
+    "Age":[28,48,59],
+    "Salary":[28500,56778,64779]
+}
+
+df=pd.DataFrame(data)
+
+print("Average Salary :")
+average_slary=df["Salary"].mean()
+print(average_slary)
