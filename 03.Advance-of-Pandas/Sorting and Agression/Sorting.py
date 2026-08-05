b0ee251196f521bce775efd3ec8a3c75f49e50a1
Muhammad Abubakar <brandmirza702@gmail.com>
2026-08-05 14:39:41 +0500

    Sorting of data

--- 03.Advance-of-Pandas/Sorting and Agression/Sorting.py
+++ 03.Advance-of-Pandas/Sorting and Agression/Sorting.py
@@ -1,2 +1,18 @@
 #Sorting method basically use for the arrang data in meaningfull way . 
 #How to sort data .
+
+
+import pandas as pd 
+
+
+data ={
+    "Name":["Omer","zabiullah","Hassan"],
+    "Age":[28,48,59],
+    "Salary":[28500,56778,64779]
+}
+
+df=pd.DataFrame(data)
+print("Orignal Data : ")
+
+print(df)
+
