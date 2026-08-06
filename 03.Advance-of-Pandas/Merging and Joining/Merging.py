a73282d9a597d0d451e63d910b6f561bb3d633dc
Muhammad Abubakar <brandmirza702@gmail.com>
2026-08-06 12:29:34 +0500

    Merging of data

--- 03.Advance-of-Pandas/Merging and Joining/Merging.py
+++ 03.Advance-of-Pandas/Merging and Joining/Merging.py
@@ -1,3 +1,18 @@
 #merge() combines DataFrames based on one or more common columns, similar to SQL JOIN.
 
 #syntax => pd.merge(left_df, right_df, on="column_name", how="join_type")
+
+import pandas as pd
+
+students = pd.DataFrame({
+    "ID": [1, 2, 3],
+    "Name": ["Ali", "Sara", "Ahmed"]
+})
+
+marks = pd.DataFrame({
+    "ID": [1, 2, 3],
+    "Marks": [85, 90, 78]
+})
+
+result = pd.merge(students, marks, on="ID")
+print(result)
