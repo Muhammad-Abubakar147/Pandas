27a54bfd7e040aba1a9aeea9bf04af25a4dded2c
Muhammad Abubakar <brandmirza702@gmail.com>
2026-08-06 12:18:06 +0500

    Grouping in data

--- 03.Advance-of-Pandas/Grouping of Data/Group.py
+++ 03.Advance-of-Pandas/Grouping of Data/Group.py
@@ -1,1 +1,13 @@
 #groupby() in Pandas is used to group rows that have the same value in one or more columns and then perform calculations on each group separately.
+
+import pandas as pd
+
+data = {
+    "Department": ["IT", "HR", "IT", "HR", "Sales"],
+    "Salary": [50000, 40000, 60000, 45000, 55000]
+}
+
+df = pd.DataFrame(data)
+
+result = df.groupby("Department")["Salary"].mean()
+print(result)
