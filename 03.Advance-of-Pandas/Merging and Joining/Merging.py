e77bac8aa380ac7faf1f51358b6ada140ef1ae5a
Muhammad Abubakar <brandmirza702@gmail.com>
2026-08-06 12:31:54 +0500

    Types of merging data

--- 03.Advance-of-Pandas/Merging and Joining/Merging.py
+++ 03.Advance-of-Pandas/Merging and Joining/Merging.py
@@ -22,3 +22,11 @@ print(result)
 # 0   1    Ali     85
 # 1   2   Sara     90
 # 2   3  Ahmed     78  output
+
+
+#Types of Data merging :
+
+# df1.join(df2, how="left")
+# df1.join(df2, how="right")
+# df1.join(df2, how="inner")
+# df1.join(df2, how="outer")
